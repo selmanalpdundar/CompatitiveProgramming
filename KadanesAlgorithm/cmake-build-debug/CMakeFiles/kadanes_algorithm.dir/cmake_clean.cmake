8eaecb4fa0141408d54cf918c0be5e4b8caebfe2
selman <selman.alp@hotmail.com.tr>
2018-10-03 22:46:24 +0200

    KadanesAlgorithm Added

---
+++ KadanesAlgorithm/cmake-build-debug/CMakeFiles/kadanes_algorithm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kadanes_algorithm.dir/main.cpp.o"
+  "kadanes_algorithm.pdb"
+  "kadanes_algorithm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kadanes_algorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ KadanesAlgorithm/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+
+    int n = 0;
+    std::cin >> n;  // n keeps how many case will be given
+
+    for (int i = 0; i < n; i++) {  // to take set of data for test
+
+        unsigned int m = 0;
+        std::cin >> m; // size of vector
+        std::vector<int> array(m);
+
+        for(int j = 0; j<m; j++){
+            std::cin>>array[j];
+        }
+
+        int sum = array[0];
+        int max = array[0];
+
+        for(int k = 1; k<m; k++){
+
+            if(sum > 0  ){
+                sum += array[k];
+            } else {
+                sum = array[k];
+            }
+
+            if(sum > max){
+                max = sum;
+            }
+        }
+
+        std::cout<<max;
+        std::cout<<std::endl;
+    }
+
+    return 0;
+}

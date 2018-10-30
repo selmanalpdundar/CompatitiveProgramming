e717aa231cee27653016c3bbda51487c7eace281
selman <selman.alp@hotmail.com.tr>
2018-10-30 21:53:26 +0100

    Megacity Added

---
+++ MegaCity/cmake-build-debug/CMakeFiles/MegaCity.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MegaCity.dir/main.cpp.o"
+  "MegaCity.pdb"
+  "MegaCity"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MegaCity.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

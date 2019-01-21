876388fc25d7c54f274ca37a595af43a1f3b24ed
selman <selman.alp@hotmail.com.tr>
2019-01-21 03:15:54 +0100

    Adding Binary search to Insertion Sort

--- Algorithms/Sort Algorithms/InsertionSort.cpp
+++ Algorithms/Sort Algorithms/InsertionSort.cpp
@@ -1,20 +1,46 @@
 #include <iostream>
 
+#define middle(low,high) ((low) + (((high) - (low)) / 2))
+
+int search(int array[], int low , int high, int element){
+    
+    while(low <= high){
+        int middle = middle(low,high);
+        
+        if(array[middle] == element){
+            return middle + 1;
+        }
+        
+        if(array[middle] < element){
+            low = middle + 1;
+        } else {
+            high = middle -1;
+        }
+    }
+    
+    return  element > array[low] ? (low+1) : low ;
+}
+
+
 void insertionSort(int array[],int size){
     int key,i ,j;
-
+    
     for(i = 1; i<size; i++){
         key = array[i];
         j = i - 1;
-
-        while(j>= 0 && array[j]> key){
+        
+        // By using binary search we decrease number of comparision  nlongn comparision
+        // But swap number is still same. n^2 swap.
+      int locationOfInsertion = search(array,0,j,key);
+        
+        while(j >= locationOfInsertion){
             array[j+1] = array[j];
-            j -= 1;
+            j --;
         }
-
+        
         array[j+1] = key;
     }
-
+    
 }
 void print(int arr[],int n){
     for(int i=0; i<n; i++){
@@ -23,11 +49,11 @@ void print(int arr[],int n){
 }
 
 int main(){
-
+    
     int array[] = {2,1,5,23,15,8,34};
     insertionSort(array,7);
     print(array,7);
-
+    
     return 0;
-
+    
 }
--- SlidingWindowMaximum.cpp
+++ SlidingWindowMaximum.cpp
@@ -1,55 +1,43 @@
 #include <iostream> 
-#include <vector> // Dynamic Array
-#include <deque>  // Double Ended Queue
+#include <vector> 
+#include <deque>
+using namespace std;
+int main() 
+{
+    int t = 0;
+    cin>>t;  
 
-int main() {
+    while(t!= 0 )
+    {
+        unsigned int n = 0;  
+        unsigned int k = 0; 
 
-    /* Problem
-         * Given an array A and an integer K.
-         * Find the maximum for each and every contiguous subarray of size K.
-     */
+        cin>>n; // Array Size
+        cin>>k; // Windows size
 
-    int t = 0; // number of test case
+        vector<int> numbers; // Stores given array
+        deque<int> deque;    // Stores windows element 
+        vector<int> result;  // Stores maximums in windows
 
-    std::cin>>t;  
-
-    while(t!= 0 ){
-
-        // ------ Declaration of Variables ----------- //
-
-        unsigned int n = 0;  // it is not going to be lower than 0
-        unsigned int k = 0;  // it is not going to be lower than 0
-
-        std::cin>>n; // Array Size
-        std::cin>>k; // Windows size
-
-        std::vector<int> numbers; // Stores given array
-        std::deque<int> deque;    // Stores windows element 
-        std::vector<int> result;  // Stores maximums in windows
-
-
-        /* To prevent descreasing of performance we reserve
-           element size therefore the vector will not copy and 
-           extend it size on runtime.
-        */
         numbers.reserve(n);
 
-
-        // Reading Number and Pusing to Vector
-        for(int i =0; i<n; i++){
-            std::cin>>numbers[i];
+        for(int i =0; i<n; i++)
+        {
+            cin>>numbers[i];
         }
 
-
-        for(int i =0; i<n; i++){
+        for(int i =0; i<n; i++)
+        {
 
             // Remove element that is not inside range of windows      
-            if(!deque.empty() && deque.front() == i - k) {
+            if(!deque.empty() && deque.front() == i - k) 
+            {
                 deque.pop_front();
             }
 
             // Remove element that smaller than current element.
-            while (!deque.empty() && numbers[deque.back()] < numbers[i]){
+            while (!deque.empty() && numbers[deque.back()] < numbers[i])
+            {
                 deque.pop_back();
             }
 
@@ -57,21 +45,21 @@ int main() {
             deque.push_back(i);
 
             // Check if i grater or equal to k - 1  push front to result array.
-            if(i >= k-1){
+            if(i >= k-1)
+            {
                 result.push_back(numbers[deque.front()]);
             }
 
         }
 
-
         // Printing result to out.
-        for(int j=0; j<result.size(); j++){
+        for(int j=0; j<result.size(); j++)
+        {
 
-            std::cout<<result[j]<<" ";
+            cout<<result[j]<<" ";
         }
-        std::cout<<std::endl;
+        cout<<endl;
 
-        // decreasing test count
         t--;
     }
 

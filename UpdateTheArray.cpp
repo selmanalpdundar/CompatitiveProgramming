2a88da7e1a9a7ed03dab201f5b23fd245d89d4f3
selman <selman.alp@hotmail.com.tr>
2019-06-14 17:57:27 +0200

    Update

--- UpdateTheArray.cpp
+++ UpdateTheArray.cpp
@@ -5,63 +5,45 @@ using namespace std;
 
 void updateBIT(vector<int> &BITree, int n, int index, int val)
 {
-    // index in BITree[] is 1 more than the index in arr[]
     index = index + 1;
     
-    // Traverse all ancestors and add 'val'
     while (index <= n)
     {
-        // Add 'val' to current node of BI Tree
         BITree[index] += val;
         
-        // Update index to that of parent in update View
         index += index & (-index);
     }
 }
 vector<int> constructBITree(vector<int> &arr, int n)
 {
-    // Create and initialize BITree[] as 0
     vector<int> BITree(n+1,0);
     
-    // Store the actual values in BITree[] using update()
     for (int i=0; i<n; i++)
         updateBIT(BITree, n, i, arr[i]);
     
-    // Uncomment below lines to see contents of BITree[]
-    //for (int i=1; i<=n; i++)
-    //      cout << BITree[i] << " ";
-    
     return BITree;
 }
-// Updates such that getElement() gets an increased
-// value when queried from l to r.
+
 void update(vector<int> &BITree, int l, int r, int n, int val)
 {
-    // Increase value at 'l' by 'val'
     updateBIT(BITree, n, l, val);
     
-    // Decrease value at 'r+1' by 'val'
     updateBIT(BITree, n, r+1, -val);
 }
 
 
-int getSum(vector<int> BITree, int index)
+int getElement(vector<int> BITree, int index)
 {
-    int sum = 0; // Iniialize result
+    int result = 0; 
     
-    // index in BITree[] is 1 more than the index in arr[]
     index = index + 1;
     
-    // Traverse ancestors of BITree[index]
     while (index>0)
     {
-        // Add current element of BITree to sum
-        sum += BITree[index];
-        
-        // Move index to parent node in getSum View
+        result += BITree[index];
         index -= index & (-index);
     }
-    return sum;
+    return result;
 }
 int main(){
     
@@ -95,7 +77,7 @@ int main(){
         while(q!= 0){
             int index = 0;
             cin>>index;
-            results.push_back(getSum(binaryIndexTree,index));
+            results.push_back(getElement(binaryIndexTree,index));
             q--;
         }
         

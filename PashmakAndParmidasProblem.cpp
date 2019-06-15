a55bae8ecc3cef893efc9e28a53bb0f71f4b5969
selman <selman.alp@hotmail.com.tr>
2019-06-15 23:09:07 +0200

    update

--- PashmakAndParmidasProblem.cpp
+++ PashmakAndParmidasProblem.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
+#include <vector>
 
-
-
-
-
+using namespace std;
 
 int main(){
+    std::ios_base::sync_with_stdio(false);
 
+    int n = 0;
+
+    cin>>n;
+
+    vector<int> inputs;
+    inputs.reserve(n);
+
+    for(int i=0; i<n; i++){
+        int number = 0;
+        cin>>number;
+        inputs.push_back(number);
+    }
     return 0;
 }
--- PowerfulArray.cpp
+++ PowerfulArray.cpp
@@ -1,12 +1,17 @@
-
+#include <vector>
 #include <iostream>
 
 
-
-
-
+using namespace std;
 
 int main(){
+    std::ios_base::sync_with_stdio(false);
+
+    int n = 0;
+    int t = 0;
+
+    cin>>n>>t;
+
 
     return 0;
 }
